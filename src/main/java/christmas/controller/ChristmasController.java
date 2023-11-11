4507047d6cc3c14cda06f8edd039586dcc682fe3
bangjaeyoung <qkdwo4613@gmail.com>
2023-11-11 15:05:32 +0900

    feat: 주문할 메뉴 이름과 개수 입력 기능 구현

--- src/main/java/christmas/controller/ChristmasController.java
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -1,8 +1,15 @@
 package christmas.controller;
 
 import christmas.domain.VisitDate;
+import christmas.domain.order.Order;
+import christmas.domain.order.Orders;
 import christmas.view.InputView;
 import christmas.view.OutputView;
+
+import java.util.List;
+
+import static christmas.domain.order.Order.NOT_VALID_MENU_ERROR_MESSAGE;
+import static christmas.view.InputView.DELIMETER_OF_MENU_NAME_AND_COUNT;
 
 public class ChristmasController {
     
@@ -16,6 +23,7 @@ public class ChristmasController {
     
     public void start() {
         VisitDate visitDate = inputVisitDate();
+        Orders orders = inputOrders();
     }
     
     private VisitDate inputVisitDate() {
@@ -24,6 +32,38 @@ public class ChristmasController {
         } catch (IllegalArgumentException e) {
             outputView.printErrorMessage(e);
             return inputVisitDate();
+        }
+    }
+    
+    private Orders inputOrders() {
+        try {
+            return new Orders(convertToOrders());
+        } catch (IllegalArgumentException e) {
+            outputView.printErrorMessage(e);
+            return inputOrders();
+        }
+    }
+    
+    private List<Order> convertToOrders() {
+        List<String> inputOrders = inputView.inputOrders();
+        return inputOrders.stream()
+                .map(inputOrder -> inputOrder.split(DELIMETER_OF_MENU_NAME_AND_COUNT))
+                .peek(this::validateMenuType)
+                .map(menu -> new Order(menu[0], convertToInt(menu[1])))
+                .toList();
+    }
+    
+    private void validateMenuType(String[] inputMenu) {
+        if (inputMenu.length != 2) {
+            throw new IllegalArgumentException(NOT_VALID_MENU_ERROR_MESSAGE);
+        }
+    }
+    
+    private int convertToInt(String userInput) {
+        try {
+            return Integer.parseInt(userInput);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(NOT_VALID_MENU_ERROR_MESSAGE);
         }
     }
 }
--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -2,15 +2,29 @@ package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static christmas.domain.order.Order.NOT_VALID_MENU_ERROR_MESSAGE;
+
 public class InputView {
     private static final String START_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
     private static final String ASK_VISIT_DATE_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    private static final String ASK_MENU_NAME_AND_COUNT_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
     private static final String NOT_VALID_DATE_ERROR_MESSAGE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+    public static final String DELIMETER_OF_MENU_NAME_AND_COUNT = "-";
     
     public int inputVisitDate() {
         printStartMessage();
         System.out.println(ASK_VISIT_DATE_MESSAGE);
         return convertToInt(getUserInput());
+    }
+    
+    public List<String> inputOrders() {
+        System.out.println(ASK_MENU_NAME_AND_COUNT_MESSAGE);
+        String userInput = getUserInput();
+        checkContainsDelimeter(userInput);
+        return convertToStringList(userInput);
     }
     
     private void printStartMessage() {
@@ -27,5 +41,15 @@ public class InputView {
     
     private String getUserInput() {
         return Console.readLine();
+    }
+    
+    private void checkContainsDelimeter(String userInput) {
+        if (!userInput.contains(DELIMETER_OF_MENU_NAME_AND_COUNT)) {
+            throw new IllegalArgumentException(NOT_VALID_MENU_ERROR_MESSAGE);
+        }
+    }
+    
+    private List<String> convertToStringList(String userInput) {
+        return Arrays.asList(userInput.split(","));
     }
 }
